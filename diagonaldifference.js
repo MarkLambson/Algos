c78dd1546b63fa9a7f9cccda7308ab69ed9e2816
MarkLambson <mj.lambson@yahoo.com>
2023-05-08 09:49:04 -0700

    diagonal difference

---
+++ diagonaldifference.js
@@ -0,0 +1,84 @@
+// https://www.hackerrank.com/challenges/diagonal-difference/problem
+/* 
+    Given a square matrix (2d array) of integers Calculate the absolute difference between the sums of its diagonals
+    The absolute difference between two variables, is simply one subtracted from the other and you get the sum.
+    - top left to bottom right diagonal
+    - top right to bottom left diagonal
+*/
+
+const squareMatrix1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [9, 8, 9],
+];
+const expected1 = 2;
+/* 
+    left to right diagonal: 1 + 5 + 9 = 15
+    right to left diagonal: 3 + 5 + 9 = 17
+    absolute difference = 2
+*/
+
+const squareMatrix2 = [
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+];
+const expected2 = 0;
+/* 
+    left to right diagonal: 1 + 2 + 3 + 4 + 5 = 15
+    right to left diagonal: 5 + 4 + 3 + 2 + 1 = 15
+    absolute difference = 0
+*/
+
+/**
+ * Calculates the absolute diagonal difference of a square matrix.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<Array<number>>} sqrMatrix A 2d array of numbers representing
+ *    a square matrix (rows and columns).
+ * @returns {number} Represents the absolute difference between the top left to
+ *    bottom right diagonal and the top right to bottom left diagonal.
+ */
+function diagonalDifference(sqrMatrix) {
+    // Code goes here
+    let sumLeft = 0
+    let sumRight = 0
+    for (let i = 0; i < sqrMatrix.length; i++) {
+        // console.log(sqrMatrix[i][i])
+        sumLeft += sqrMatrix[i][i]
+    }
+    let count = sqrMatrix.length - 1
+    for (let j = 0; j < sqrMatrix.length; j++) {
+        sumRight += sqrMatrix[j][count]
+        count--
+    }
+    return Math.abs(sumLeft - sumRight)
+}
+console.log(diagonalDifference(squareMatrix1));
+console.log(diagonalDifference(squareMatrix2));
+
+//----------------------------------------------------------------------------------------------------------------
+
+function functionalDiagonalDifference(sqrMatrix) {
+    let lTRSum = 0;
+    let rTLSum = 0;
+    let arr = sqrMatrix.flat()
+    for (let i = 0; i < arr.length; i += sqrMatrix.length + 1) {
+        lTRSum += arr[i]
+    }
+    for (let j = sqrMatrix[0].length - 1; j < arr.length - sqrMatrix[0].length + 1; j += sqrMatrix.length - 1) {
+
+        rTLSum += arr[j]
+    }
+    let total = lTRSum - rTLSum
+    return Math.abs(total)
+}
+
+
+console.log(diagonalDifference(squareMatrix1));
+console.log(diagonalDifference(squareMatrix2));
+
+/*****************************************************************************/
+// module.exports = { diagonalDifference, functionalDiagonalDifference };
