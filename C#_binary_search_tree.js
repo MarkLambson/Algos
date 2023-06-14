a2a600b7e6570f03c6a4ac8a1d35887fc285cd87
MarkLambson <mj.lambson@yahoo.com>
2023-06-14 08:00:55 -0700

    range, contains, containsrecursive

--- C#_binary_search_tree.js
+++ C#_binary_search_tree.js
@@ -39,6 +39,92 @@ class BinarySearchTree {
     }
 //⁡⁢⁣⁢----------------------------------------------BELOW THIS POINT ARE FUNCTIONS----------------------------------------------//⁡
 
+    /**
+     * Inserts a new node with the given newVal in the right place to preserver
+     * the order of this tree.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {number} newVal The data to be added to a new node.
+     * @returns {BinarySearchTree} This tree.
+     */
+    insert(newVal) { }
+
+    /**
+     * Inserts a new node with the given newVal in the right place to preserver
+     * the order of this tree.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {number} newVal The data to be added to a new node.
+     * @param {Node} curr The node that is currently accessed from the tree as
+     *    the tree is being traversed.
+     * @returns {BinarySearchTree} This tree.
+     */
+    insertRecursive(newVal, curr = this.root) { }
+
+    /**
+     * Determines if this tree contains the given searchVal.
+     * - Time: O(h) linear, h = height of tree.
+     * - Space: O(1) constant.
+     * @param {number} searchVal The number to search for in the node's data.
+     * @returns {boolean} Indicates if the searchVal was found.
+     */
+    contains(searchVal) {
+        let current = this.root;
+
+        while (current) {
+            if (current.data === searchVal) {
+                return true;
+            }
+
+            if (searchVal < current.data) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Determines if this tree contains the given searchVal.
+     * - Time: O(h) linear, h = height of tree.
+     * - Space: O(h) linear due to the call stack.
+     * @param {number} searchVal The number to search for in the node's data.
+     * @returns {boolean} Indicates if the searchVal was found.
+     */
+    containsRecursive(searchVal, current = this.root) {
+        if (current === null) {
+            return false;
+        }
+
+        if (current.data === searchVal) {
+            return true;
+        }
+
+        if (searchVal < current.data) {
+            return this.containsRecursive(searchVal, current.left);
+        }
+
+        if (searchVal > current.data) {
+            return this.containsRecursive(searchVal, current.right);
+        }
+    }
+
+    /**
+     * Calculates the range (max - min) from the given startNode.
+     * - Time: O(rightHeight + leftHeight) -> still linear so simplify to O(h).
+     * - Space: O(h) linear due to the call stack. The max side finishes before
+     *    the right min side is added to the stack.
+     * @param {BSTNode} startNode The node to start from to calculate the range.
+     * @returns {number|null} The range of this tree or a sub tree depending on if the
+     *    startNode is the root or not.
+     */
+    range(startNode = this.root) {
+        if (!startNode) {
+            return null;
+        }
+        return this.max(startNode) - this.min(startNode);
+    }
 
     /**
      * Determines if this tree is empty.
@@ -213,31 +299,41 @@ threeLevelTree.root.right.left = new BSTNode(13);
   //   .insert(66)
   //   .insert(90);
 
+console.log("---------Print Trees---------")
 emptyTree.print()
 oneNodeTree.print()
 twoLevelTree.print()
 threeLevelTree.print()
+console.log("-------------------------------")
 
-console.log(emptyTree.isEmpty())
-console.log(emptyTree.min())
-console.log(emptyTree.max())
-console.log(emptyTree.minRecursive())
-console.log(emptyTree.maxRecursive())
+// console.log("---------emptyTree empty, max, min, recursives---------")
+// console.log(emptyTree.isEmpty())
+// console.log(emptyTree.min())
+// console.log(emptyTree.max())
+// console.log(emptyTree.minRecursive())
+// console.log(emptyTree.maxRecursive())
+// console.log("-------------------------------")
 
-console.log(oneNodeTree.isEmpty())
-console.log(oneNodeTree.min())
-console.log(oneNodeTree.max())
-console.log(oneNodeTree.minRecursive())
-console.log(oneNodeTree.maxRecursive())
+// console.log("---------oneNodeTree empty, max, min, recursives---------")
+// console.log(oneNodeTree.isEmpty())
+// console.log(oneNodeTree.min())
+// console.log(oneNodeTree.max())
+// console.log(oneNodeTree.minRecursive())
+// console.log(oneNodeTree.maxRecursive())
+// console.log("-------------------------------")
 
-console.log(twoLevelTree.isEmpty())
-console.log(twoLevelTree.min())
-console.log(twoLevelTree.max())
-console.log(twoLevelTree.minRecursive())
-console.log(twoLevelTree.maxRecursive())
+// console.log("---------twoLevelTree empty, max, min, recursives---------")
+// console.log(twoLevelTree.isEmpty())
+// console.log(twoLevelTree.min())
+// console.log(twoLevelTree.max())
+// console.log(twoLevelTree.minRecursive())
+// console.log(twoLevelTree.maxRecursive())
+// console.log("-------------------------------")
 
-console.log(threeLevelTree.isEmpty())
-console.log(threeLevelTree.min())
-console.log(threeLevelTree.max())
-console.log(threeLevelTree.minRecursive())
-console.log(threeLevelTree.maxRecursive())
+// console.log("---------threeLevelTree empty, max, min, recursives---------")
+// console.log(threeLevelTree.isEmpty())
+// console.log(threeLevelTree.min())
+// console.log(threeLevelTree.max())
+// console.log(threeLevelTree.minRecursive())
+// console.log(threeLevelTree.maxRecursive())
+// console.log("-------------------------------")
