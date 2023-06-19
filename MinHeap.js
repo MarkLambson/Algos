36808d2e0c04143290c3f36064c3d0b067e7b3c8
MarkLambson <mj.lambson@yahoo.com>
2023-06-19 09:11:49 -0700

    top and insert

---
+++ MinHeap.js
@@ -0,0 +1,84 @@
+/**
+ * Class to represent a MinHeap which is a Priority Queue optimized for fast
+ * retrieval of the minimum value. It is implemented using an array but it is
+ * best visualized as a tree structure where each 'node' has left and right
+ * children except the 'node' may only have a left child.
+ * - parent is located at: Math.floor(i / 2);
+ * - left child is located at: i * 2
+ * - right child is located at: i * 2 + 1
+ */
+class MinHeap {
+    constructor() {
+        /**
+         * 0th index not used, so null is a placeholder. Not using 0th index makes
+         * calculating the left and right children's index cleaner.
+         * This also effectively makes our array start at index 1.
+         */
+        this.heap = [null];
+    }
+
+    /**
+     * Retrieves the top (minimum number) in the heap without removing it.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {?number} Null if empty.
+     */
+    top() {
+        if (this.heap.length > 1) {
+            return this.heap[1];
+        }
+        return null;
+    }
+
+    /**
+     * Inserts a new number into the heap and maintains the heaps order.
+     * 1. Push new num to back then.
+     * 2. Iteratively swap the new num with it's parent while it is smaller than
+     *    it's parent.
+     * - Time: O(log n) logarithmic due to shiftUp / iterative swapping.
+     * - Space: O(1) constant.
+     * @param {number} num The num to add.
+     */
+    insert(num) {
+        this.heap.push(num);
+        if (this.heap.length > 1) {
+            let idx = this.heap.length - 1;
+            while (idx > 1 && this.heap[Math.floor(idx / 2)] > this.heap[idx]) {
+                let temp = this.heap[Math.floor(idx / 2)];
+                this.heap[Math.floor(idx / 2)] = this.heap[idx];
+                this.heap[idx] = temp;
+                idx = Math.floor(idx / 2);
+            }
+        }
+        return num;
+    }
+
+    /**
+     * Logs the tree horizontally with the root on the left and the index in
+     * parenthesis using reverse inorder traversal.
+     */
+    printHorizontalTree(parentIdx = 1, spaceCnt = 0, spaceIncr = 10) {
+        if (parentIdx > this.heap.length - 1) {
+            return;
+        }
+
+        spaceCnt += spaceIncr;
+        this.printHorizontalTree(parentIdx * 2 + 1, spaceCnt);
+
+        console.log(
+            " ".repeat(spaceCnt < spaceIncr ? 0 : spaceCnt - spaceIncr) +
+            `${this.heap[parentIdx]} (${parentIdx})`
+        );
+
+        this.printHorizontalTree(parentIdx * 2, spaceCnt);
+    }
+}
+
+const minTree = new MinHeap;
+console.log(minTree.insert(14));
+console.log(minTree.insert(15));
+console.log(minTree.insert(12));
+console.log(minTree.insert(13));
+console.log(minTree.insert(16));
+console.log(minTree.insert(11));
+console.log(minTree.printHorizontalTree());
