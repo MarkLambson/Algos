42e62d2ddd380b4ae7a3fc6ccc42aff3708fd064
MarkLambson <mj.lambson@yahoo.com>
2023-06-28 08:38:41 -0700

    palindrome, compare

--- C#_queue.js
+++ C#_queue.js
@@ -7,7 +7,36 @@ class Queue {
     constructor() {
         this.items = [];
     }
-//⁡⁢⁣⁢-------------------------------------------------------------------------------------⁡//
+
+    /**
+     * Compares this queue to another given queue to see if they are equal.
+     * Avoid indexing the queue items directly via bracket notation, use the
+     * queue methods instead for practice.
+     * Use no extra array or objects.
+     * The queues should be returned to their original order when done.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Queue} q2 The queue to be compared against this queue.
+     * @returns {boolean} Whether all the items of the two queues are equal and
+     *    in the same order.
+     */
+    compareQueues(q2) {
+
+    }
+
+    /**
+     * Determines if the queue is a palindrome (same items forward and backwards).
+     * Avoid indexing queue items directly via bracket notation, instead use the
+     * queue methods for practice.
+     * Use only 1 stack as additional storage, no other arrays or objects.
+     * The queue should be returned to its original order when done.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @returns {boolean}
+     */
+    isPalindrome() {
+
+    }
 
     /**
      * Adds a new given item to the back of this queue.
@@ -17,7 +46,8 @@ class Queue {
      * @returns {number} The new size of this queue.
      */
     enqueue(item) {
-
+        this.items.push(item);
+        return this.size();
     }
 
     /**
@@ -28,7 +58,7 @@ class Queue {
      * @returns {any} The first item or undefined if empty.
      */
     dequeue() {
-
+        return this.items.shift();
     }
 
     /**
@@ -38,7 +68,7 @@ class Queue {
      * @returns {any} The first item or undefined if empty.
      */
     front() {
-
+        return this.items[0];
     }
 
     /**
@@ -48,7 +78,7 @@ class Queue {
      * @returns {boolean}
      */
     isEmpty() {
-
+        return this.items.length === 0;
     }
 
     /**
@@ -58,7 +88,7 @@ class Queue {
      * @returns {number} The length.
      */
     size() {
-
+        return this.items.length;
     }
 }
 
